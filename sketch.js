9103f27337cf3d49fcefbfdb8f11869e617227d2
Bénédicte Pignol <bpignol@users.noreply.github.com>
2018-02-22 13:40:58 +0100

    update sketch.js comment

--- sketch.js
+++ sketch.js
@@ -6,7 +6,12 @@ function setup(){
 }
 
 function draw(){
+  // mouseX : changes based on the X position of the mouse
+  // map : numerical variable to listen to, min value, max value, min output, max output)
+  // myVar : always changing value
   var myVar=map(mouseX,0,400,0,255);
+  console.log(myVar)
+   // fill and stroke use myVar as the red element of their RGB definition
   fill(myVar,10,65);
   stroke(myVar,56,180);
   strokeWeight(10)
